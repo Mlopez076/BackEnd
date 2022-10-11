357b8022d0a88e56d2bb798e851d27417b240cfb
Edgartovarx3 <escobadoo97@gmail.com>
2022-10-11 10:13:16 -0500

    Catalogos

---
+++ src/main/java/DataLayer/Catalogos.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DataLayer;
+
+/**
+ *
+ * @author Edgar Garibay
+ */
+public class Catalogos {
+    
+    String[] Catalogos;
+    
+    int[] ID;
+    
+    public void LlenarCatalogos(){
+    for(int i=0; i>ID.length; i++){
+        
+        for(int e=0; e>Catalogos.length;e++){
+        }
+    }
+    }
+}
