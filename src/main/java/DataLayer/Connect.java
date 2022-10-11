99963fa5dbac1db7e0a77e74dcbf6fb2b7f5005e
Edgartovarx3 <escobadoo97@gmail.com>
2022-10-11 10:33:59 -0500

    g

--- src/main/java/DataLayer/Connect.java
+++ src/main/java/DataLayer/Connect.java
@@ -13,25 +13,21 @@ public class Connect {
 
   public static void main (String args []) throws SQLException
   {
-    try 
-    {
-     Class.forName ("oracle.jdbc.driver.OracleDriver");
-    } 
-    catch (ClassNotFoundException e) 
-    {
-     e.printStackTrace();
-    }
+    
+
+    DriverManager.registerDriver (new oracle.jdbc.driver.OracleDriver());
 
     Connection conn = DriverManager.getConnection
-         ("jdbc:oracle:oci:@ORACLEBD", "user", "passw");
-          //               @TNSNames_Entry,  userid,  password
+          ("jdbc:oracle:thin:@WIN01:1521:oracleBD", "user", "passw");
+         // driver@machineName:port:SID           ,  userid,  password
 
     Statement stmt = conn.createStatement();
     ResultSet rset = 
               stmt.executeQuery("select BANNER from SYS.V_$VERSION");
     while (rset.next())
-          System.out.println (rset.getString(1));   // Print col 1
+         System.out.println (rset.getString(1));   // Print col 1
     stmt.close();
-  }
- }
+    
 
+  }
+ } 
