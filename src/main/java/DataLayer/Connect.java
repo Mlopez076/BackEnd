6d246141cdb26ddea10ef82cdb90e7963f1010bf
Edgartovarx3 <escobadoo97@gmail.com>
2022-10-18 09:35:12 -0500

    Conexion bd
    La conexion ah sido añadida

--- src/main/java/DataLayer/Connect.java
+++ src/main/java/DataLayer/Connect.java
@@ -3,22 +3,26 @@ package DataLayer;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import oracle.jdbc.OracleResultSet;
+import oracle.jdbc.OraclePreparedStatement;
+import java.util.Properties;
 import javax.swing.JOptionPane;
 
 public class Connect {
 
     private static Connection conn = null;
-    private static String login = "Prueba";
-    private static String clave = "234509";
-    private static String url = "jdbbc:oracle:thin:@localhost:1521";
+    private static String login = "Backend";
+    private static String clave = "12345";
+    private static String url = "jdbc:oracle:thin:@25.37.209.77:1521:XE";
 
     public static void main(String[] args) {
         Connect c = new Connect();
+        getConnection();
     }
 
     public static Connection getConnection() {
         try {
-            Class.forName("oracle.jdbc.driver.Oracle Driver");
+          //  Class.forName("oracle.jdbc.driver.Oracle Driver");
             conn = DriverManager.getConnection(url, login, clave);
             conn.setAutoCommit(false);
             if (conn != null) {
@@ -27,10 +31,10 @@ public class Connect {
                 System.out.println("Conexion erronea");
             }
 
-        } catch (ClassNotFoundException | SQLException e) {
-
-            JOptionPane.showMessageDialog(null, "Conexion erronea");
-        }
+        } catch (SQLException e) {
+                 System.out.println(e);
+            JOptionPane.showMessageDialog(null, "Conexion erronea: "+e);
+        } 
         return conn;
 
     }
