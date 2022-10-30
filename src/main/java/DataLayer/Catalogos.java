a2b906baeadac037c76aaa2c28bf4e7b4cbf11df
Edgartovarx3 <escobadoo97@gmail.com>
2022-10-30 09:26:58 -0600

    a

--- src/main/java/DataLayer/Catalogos.java
+++ src/main/java/DataLayer/Catalogos.java
@@ -3,11 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package DataLayer;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-import java.sql.SQLException;
-import javax.swing.JOptionPane;
+
 
 /**
  *
@@ -26,36 +22,8 @@ public class Catalogos {
         for(int e=0; e>Catalogos.length;e++){
         }
     }
-
-
-
-public class MetodosBD {
-    
-  Statement stm= null;
-   Connection con=null;
-    boolean registrar=false;
-    public void registrar(Usuarios cliente){
-    
-        String sql="INSERT INTO cliente values (NULL,'"+cliente.getNombre()+"','"+cliente.getApellidoPa()+"','"+cliente.getApellidoMa()+"')";
-        
-        try {			
-			con =Connect.getConnection();
-			stm= con.createStatement();
-			stm.execute(sql);
-			registrar=true;
-			stm.close();
-			con.close();
-                        
-                        if(registrar==true){
-                            System.out.println("Registro Exitoso");
-                        }else{
-                            System.out.println("Error al registrar");
-                        }
-		} catch (SQLException e) {
-			System.out.println("Error: Clase ClienteDaoImple, método registrar");
-			e.printStackTrace();
-		}
-		
-
     }
+
+
+
 }
